ace8ad68bf0f9bf364093da677b8be0ffe26b9be
hypericumimpex <hypericumimpex.ro@gmail.com>
2019-06-18 08:00:05 +0300

    Update to 2.4.10.3

--- gravityforms.php
+++ gravityforms.php
@@ -3,7 +3,7 @@
 Plugin Name: HYP Forms
 Plugin URI: https://github.com/hypericumimpex/hyp-forms/
 Description: Easily create web forms and manage form entries within the WordPress admin.
-Version: 2.4.10.1
+Version: 2.4.10.3
 Author: Romeo C.
 Author URI: https://github.com/hypericumimpex/
 License: GPL-2.0+
@@ -215,7 +215,7 @@ class GFForms {
 	 *
 	 * @var string $version The version number.
 	 */
-	public static $version = '2.4.10.1';
+	public static $version = '2.4.10.3';
 
 	/**
 	 * Handles background upgrade tasks.
@@ -586,7 +586,8 @@ class GFForms {
 	 * @return array $plugins Supported plugins.
 	 */
 	public static function set_logging_supported( $plugins ) {
-		$plugins['gravityforms'] = 'Gravity Forms Core';
+		$plugins['gravityformsapi'] = 'Gravity Forms API';
+		$plugins['gravityforms']    = 'Gravity Forms Core';
 
 		return $plugins;
 	}
